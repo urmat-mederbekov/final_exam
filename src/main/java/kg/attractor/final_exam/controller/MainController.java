6b2a163ec35b1ca61bae2c18e910145036ada467
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-08-15 17:21:02 +0600

    search get method added

--- src/main/java/kg/attractor/final_exam/controller/MainController.java
+++ src/main/java/kg/attractor/final_exam/controller/MainController.java
@@ -1,15 +1,20 @@
 package kg.attractor.final_exam.controller;
 
 import kg.attractor.final_exam.form.UserForm;
+import kg.attractor.final_exam.service.ImageService;
+import kg.attractor.final_exam.service.PropertiesService;
 import kg.attractor.final_exam.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.security.Principal;
 
@@ -18,11 +23,26 @@ import java.security.Principal;
 public class MainController {
 
     private final UserService userService;
+    private final ImageService imageService;
+    private final PropertiesService propertiesService;
 
     @GetMapping
-    public String indexPage(Model model, Principal principal){
+    public String indexPage(Model model, Principal principal, Pageable pageable, HttpServletRequest uriBuilder){
 
         userService.checkUserPresence(model, principal);
+        PropertiesService.constructPageable(imageService.getAllImagesById(pageable),
+                propertiesService.getDefaultPageSize(), model, uriBuilder.getRequestURI());
+        return "index";
+    }
+
+    @GetMapping("/search")
+    public String indexPage(@RequestParam String name, Model model, Principal principal,
+                           Pageable pageable, HttpServletRequest uriBuilder){
+
+        userService.checkUserPresence(model, principal);
+        PropertiesService.constructPageable(imageService.search(name, pageable),
+                propertiesService.getDefaultPageSize(), model, uriBuilder.getRequestURI());
+
         return "index";
     }
 
