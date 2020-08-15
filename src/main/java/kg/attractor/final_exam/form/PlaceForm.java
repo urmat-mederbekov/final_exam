3ca7d3f2cce93505266cdea27ab8735f7baf2d3b
urmat_mederbekov <urmat.mederbekov@gmail.com>
2020-08-15 17:22:55 +0600

    field MultipartFile image added

--- src/main/java/kg/attractor/final_exam/form/PlaceForm.java
+++ src/main/java/kg/attractor/final_exam/form/PlaceForm.java
@@ -2,12 +2,9 @@ package kg.attractor.final_exam.form;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.constraints.Future;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -18,4 +15,6 @@ public class PlaceForm {
 
     @NotBlank(message = "Обязательное поле")
     private String description;
+
+    private MultipartFile image;
 }
